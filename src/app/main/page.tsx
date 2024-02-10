19406408d05518d63029d6267f54385ed5784b0b
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2024-02-11 03:23:45 +0530

    Final Commit

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,32 +2,8 @@
 import Ai from "@/components/ui/main/ai";
 import Body from "@/components/ui/main/body";
 import Hero from "@/components/ui/main/hero";
-import { useEffect } from "react";
 
 export default function Page() {
-	useEffect(() => {
-		const item = localStorage.getItem("img");
-		const blobText = item ? JSON.parse(item) : null;
-
-		if (blobText) {
-			const filename = "image-" + Date.now() + ".jpg";
-
-			const imageUrl = URL.createObjectURL(blobText);
-
-			const link = document.createElement("a");
-			link.href = imageUrl;
-			link.download = filename;
-			console.log(link);
-
-			link.click();
-
-			URL.revokeObjectURL(imageUrl);
-		} else {
-			// Handle the case where there is no data in localStorage
-			alert("No image data found in localStorage");
-		}
-	}, []);
-
 	return (
 		<div className="flex flex-col mx-[5vw] ">
 			<Hero />
